66c1527c6eff3158cb8ac2faddd7219c89ec6916
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2020-04-18 13:40:14 +0900

    only pip, not use of anaconda

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,13 +5,14 @@ source scripts/settings.sh
 # v4l2loopback
 sudo apt install v4l2loopback-dkms
 
-source $(conda info --base)/etc/profile.d/conda.sh
-conda create -y -n $CONDA_ENV_NAME python=3.8
-conda activate $CONDA_ENV_NAME
+# original env is conda
+# source $(conda info --base)/etc/profile.d/conda.sh
+# conda create -y -n $CONDA_ENV_NAME python=3.8
+# conda activate $CONDA_ENV_NAME
 
-pip install face-alignment pyfakewebcam
+pip3 install face-alignment pyfakewebcam
 
 # FOMM
 git clone https://github.com/alievk/first-order-model.git fomm
-pip install -r fomm/requirements.txt
-pip install requests
+pip3 install -r fomm/requirements.txt
+pip3 install requests
